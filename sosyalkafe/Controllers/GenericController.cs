048d3d71dde2f50415e20e3d77c0dcbb337e1af0
ugurozturk <tarantulaugur@hotmail.com>
2016-11-18 14:17:26 +0300

    Filtrelemeler eklendi

--- sosyalkafe/Controllers/GenericController.cs
+++ sosyalkafe/Controllers/GenericController.cs
@@ -27,6 +27,8 @@ namespace sosyalkafe.Controllers
             {
                 firmaAdi = firmaadi;
             }
+
+            
             string regexi = "\"likes\": (.*?) \"is_video\":";
             string likecountregexi = "\"likes\": {\"count\": (.*?)}";
             string thumbimageregexi = "thumbnail_src\": \"(.*?)\"";
@@ -72,36 +74,62 @@ namespace sosyalkafe.Controllers
             List<Tuple<string, string>> listem = new List<Tuple<string, string>>();
             MatchCollection mth = Regex.Matches(data, regexi);
 
+
+            //Filtreleme ve Değerlendirme
             foreach (Match item in mth)
             {
                 MatchCollection likecount = Regex.Matches(item.Groups[0].Value, likecountregexi);
                 MatchCollection thumbimage = Regex.Matches(item.Groups[0].Value, thumbimageregexi);
 
-                //TODO sunucuyu filtrele
-                //TODO yoksa ekle, varsa ekleme
-                listem.Add(new Tuple<string, string>(
-                    likecount[0].Groups[1].Value,
-                    thumbimage[0].Groups[1].Value 
-                    ));
-            }
+                int ilikecount = 0;
+                int.TryParse(likecount[0].Groups[1].Value, out ilikecount);
 
-            foreach (Tuple<string,string> veri in listem)
-            {
-                int likecount = 0;
-                int.TryParse(veri.Item1, out likecount);
-
-                if (likecount > 10)
+                if (ilikecount > 10) //Like sayısı 10 dan fazla ise
                 {
-                    musteri_gonderileri mgonderi = new musteri_gonderileri();
-                    mgonderi.musteri_id = 1; //TODO sonradan dinamik hale getir.
-                    mgonderi.resim_adres = veri.Item2;
-                    mgonderi.aktif = 1; //TODO Kafenin isteğine bağlı olacak
-                    mgonderi.firma_kodlari_id = firmaKodlari.firma_kodlari_id;
+                    bool resimVarmiSonuc = resimVarmi(thumbimage[0].Groups[1].Value);
 
-                    ent.musteri_gonderileri.Add(mgonderi);
-                    ent.SaveChanges();
+                    if (resimVarmiSonuc)
+                    {
+                        continue; //Resim Varsa eklemeyi atla.
+                    }
+
+                    listem.Add(new Tuple<string, string>(
+                        likecount[0].Groups[1].Value,
+                        thumbimage[0].Groups[1].Value
+                        ));
                 }
             }
+
+
+            //Verileri veritabanına yazdırma
+            foreach (Tuple<string, string> veri in listem)
+            {
+                musteri_gonderileri mgonderi = new musteri_gonderileri();
+                mgonderi.musteri_id = 1; //TODO sonradan dinamik hale getir.
+                mgonderi.resim_adres = veri.Item2;
+                mgonderi.aktif = 1; //TODO Kafenin isteğine bağlı olacak
+                mgonderi.firma_kodlari_id = firmaKodlari.firma_kodlari_id;
+
+                ent.musteri_gonderileri.Add(mgonderi);
+                ent.SaveChanges();
+            }
+        }
+
+        public bool resimVarmi(string resimAdres)
+        {
+            //Farklı Sunucudan çekilen aynı resimlerin çakılması önlendi.
+            resimAdres = resimAdres.Substring(resimAdres.IndexOf("cdninstagram.com"), resimAdres.LastIndexOf("?ig_cache_key"));
+            
+            var veri = (from a in ent.musteri_gonderileri
+                        where a.resim_adres.Contains(resimAdres)
+                        select a).FirstOrDefault();
+
+            if (veri == null)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
