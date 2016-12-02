4cf95c5586139edb9e4d54a6ef106fe2d8532b28
ugurozturk <tarantulaugur@hotmail.com>
2016-12-03 00:05:19 +0300

    Asımdan Alındı

---
+++ sosyalkafe/Areas/Admin/Controllers/AyarlarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sosyalkafe.Areas.Admin.Controllers
+{
+    public class AyarlarController : Controller
+    {
+        // GET: Admin/Ayarlar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
